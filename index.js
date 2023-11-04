d9ca6e362399c692a326fea2789d39639d4f7127
TrungLe <trungle9995@gmail.com>
2023-11-04 15:35:38 -0400

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Jack Takae',
+    streetAddress: '329 Willow St'
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value)
+{
+    const copyArray = {...employee}
+    copyArray[key] = value
+    
+    return copyArray
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value)
+{
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key)
+{
+    const copyClone = {...employee}
+    delete copyClone[key]
+    return copyClone
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key)
+{
+    delete employee[key]
+    return employee
+}
